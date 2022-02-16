b7e82fda50a66fd1776fac8585fe58dc1efa3f9b
Tom Phillips <tomphillipsmusic@gmail.com>
2022-02-16 07:20:18 -0500

    Initial Commit

---
+++ TapIt/TapItApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapItApp.swift
+//  TapIt
+//
+//  Created by Tom Phillips on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct TapItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
